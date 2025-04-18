306ff7a2e2e8d661cfd77eb188632d9f0fa21ae7
iwrawww@gmail.com <iwrawww@gmail.com>
2025-04-18 18:05:44 +0700

    гуд

--- TreeNode.cs
+++ TreeNode.cs
@@ -2,13 +2,13 @@
 
 public class TreeNode<T> where T : IComparable<T>
 {
-    public TreeNode(T data)
-    {
-        Data = data;
-    }
+  public TreeNode(T data)
+  {
+    Data = data;
+  }
 
-    public TreeNode<T> Parent { get; set; }
-    public T Data { get; set; }
-    public TreeNode<T> Left { get; set; }
-    public TreeNode<T> Right { get; set; }
+  public TreeNode<T> Parent { get; set; }
+  public T Data { get; set; }
+  public TreeNode<T> Left { get; set; }
+  public TreeNode<T> Right { get; set; }
 }
