f07d7683f72c113aad419963b044c71ec4bb0702
iwrawww@gmail.com <iwrawww@gmail.com>
2025-04-18 17:44:48 +0700

    добавляю классы

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace lab7CS
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        BinaryTree<int> tree = new BinaryTree<int>();
+        tree.Add(5);
+        tree.Add(3);
+        tree.Add(8);
+        tree.Add(9);
+        tree.Add(10);
+        tree.Add(11);
+
+        Func<BinaryTree<int>, IEnumerable<int>> inorder = t => t.InOrderTraversal();
+
+        Console.WriteLine("In-order traversal:");
+        foreach (var number in inorder(tree))
         {
+            Console.WriteLine(number);
         }
     }
 }
